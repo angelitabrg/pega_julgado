8630f4320be8cc73667fe50f9ada0e59d60b2a73
Angelita Guedes <angelita.brg@outlook.com>
2024-01-28 16:01:49 -0300

    Add next page functionality.

--- esaj/esaj/spiders/cjsg.py
+++ esaj/esaj/spiders/cjsg.py
@@ -2,11 +2,27 @@ import scrapy
 
 from esaj.spiders.helper.innertext import innertext_quick
 from esaj.spiders.helper.treatment import treatment
+from scrapy_splash import SplashRequest
+
+next_page_script = """
+function main(splash, args)
+    splash:go(args.url)
+
+    local a_element = splash:select('[title="Próxima página"]')
+    a_element:mouse_click()
+
+    splash:wait(splash.args.wait)  
+    return splash:html()
+end
+"""
 
 class CjsgSpider(scrapy.Spider):
     name = "cjsg"
     allowed_domains = ["esaj.tjsp.jus.br"]
-    start_urls = ["https://esaj.tjsp.jus.br/cjsg/resultadoCompleta.do?conversationId=&dados.buscaInteiroTeor=vazamento&dados.pesquisarComSinonimos=S&dados.pesquisarComSinonimos=S&dados.buscaEmenta=&dados.nuProcOrigem=&dados.nuRegistro=&agenteSelectedEntitiesList=&contadoragente=0&contadorMaioragente=0&codigoCr=&codigoTr=&nmAgente=&juizProlatorSelectedEntitiesList=&contadorjuizProlator=0&contadorMaiorjuizProlator=0&codigoJuizCr=&codigoJuizTr=&nmJuiz=&classesTreeSelection.values=&classesTreeSelection.text=&assuntosTreeSelection.values=&assuntosTreeSelection.text=&comarcaSelectedEntitiesList=&contadorcomarca=0&contadorMaiorcomarca=0&cdComarca=&nmComarca=&secoesTreeSelection.values=&secoesTreeSelection.text=&dados.dtJulgamentoInicio=&dados.dtJulgamentoFim=&dados.dtPublicacaoInicio=&dados.dtPublicacaoFim=&dados.origensSelecionadas=T&tipoDecisaoSelecionados=A&dados.ordenarPor=dtPublicacao"]
+
+    def start_requests(self):
+        url = "https://esaj.tjsp.jus.br/cjsg/resultadoCompleta.do?conversationId=&dados.buscaInteiroTeor=vazamento&dados.pesquisarComSinonimos=S&dados.pesquisarComSinonimos=S&dados.buscaEmenta=&dados.nuProcOrigem=&dados.nuRegistro=&agenteSelectedEntitiesList=&contadoragente=0&contadorMaioragente=0&codigoCr=&codigoTr=&nmAgente=&juizProlatorSelectedEntitiesList=&contadorjuizProlator=0&contadorMaiorjuizProlator=0&codigoJuizCr=&codigoJuizTr=&nmJuiz=&classesTreeSelection.values=&classesTreeSelection.text=&assuntosTreeSelection.values=&assuntosTreeSelection.text=&comarcaSelectedEntitiesList=&contadorcomarca=0&contadorMaiorcomarca=0&cdComarca=&nmComarca=&secoesTreeSelection.values=&secoesTreeSelection.text=&dados.dtJulgamentoInicio=&dados.dtJulgamentoFim=&dados.dtPublicacaoInicio=&dados.dtPublicacaoFim=&dados.origensSelecionadas=T&tipoDecisaoSelecionados=A&dados.ordenarPor=dtPublicacao"
+        yield SplashRequest(url, self.parse, args={'wait': 1})
 
     def parse(self, response):
         for process in response.css('#tdResultados .fundocinza1'):
@@ -17,22 +33,33 @@ class CjsgSpider(scrapy.Spider):
                 'data_julgamento': self.get_detail(process, 'Data do julgamento:'),
                 'data_publicacao': self.get_detail(process, 'Data de publicação:'),
             }
+        if self.has_next_page(response) is not None:
+            yield SplashRequest(
+                response.url,
+                callback=self.parse,
+                endpoint='execute',
+                dont_filter=True,
+                args={'wait': 2, 'lua_source': next_page_script, 'url': response.url}
+            )
+
+    def has_next_page(self, response):
+        if response.css('[title="Próxima página"]'):
+            return True
+
+    def get_current_page(self, response):
+        return int(response.css('.paginaAtual::text').get().strip())
 
     def get_occurrence_number(self, process):
         text = process.css('.segredoJustica::text').get(default="").strip()
         pos = text.find(' ocorrência')
-
         occurrence_number = text[1:pos]
-
         if occurrence_number:
             return occurrence_number
-
         return ''
 
     def get_detail(self, process, search, css_selector=''):
         element = process.css(f'.ementaClass2 {css_selector}:contains("{search}")')
         text = innertext_quick(element)[0]
-
         pos = len(search)
         final_text = text[pos:]
         if final_text:
