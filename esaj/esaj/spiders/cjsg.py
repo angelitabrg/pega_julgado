c436d9b0876f1d07097fe4a335a0e9d934ea5ae1
Angelita Guedes <angelita.brg@outlook.com>
2024-02-09 17:12:09 -0300

    Fix cjsg

--- esaj/esaj/spiders/cjsg.py
+++ esaj/esaj/spiders/cjsg.py
@@ -1,47 +1,56 @@
+from time import sleep
+
 import scrapy
-import pdb
+import urllib.parse
+import logging
 
 from esaj.spiders.helper.innertext import innertext_quick
 from esaj.spiders.helper.treatment import treatment
-from scrapy_splash import SplashRequest
+
 
 class CjsgSpider(scrapy.Spider):
     name = "cjsg"
     allowed_domains = ["esaj.tjsp.jus.br"]
 
     def start_requests(self):
-        url = "https://esaj.tjsp.jus.br/cjsg/resultadoCompleta.do?conversationId=&dados.buscaInteiroTeor=vazamento&dados.pesquisarComSinonimos=S&dados.pesquisarComSinonimos=S&dados.buscaEmenta=&dados.nuProcOrigem=&dados.nuRegistro=&agenteSelectedEntitiesList=&contadoragente=0&contadorMaioragente=0&codigoCr=&codigoTr=&nmAgente=&juizProlatorSelectedEntitiesList=&contadorjuizProlator=0&contadorMaiorjuizProlator=0&codigoJuizCr=&codigoJuizTr=&nmJuiz=&classesTreeSelection.values=&classesTreeSelection.text=&assuntosTreeSelection.values=&assuntosTreeSelection.text=&comarcaSelectedEntitiesList=&contadorcomarca=0&contadorMaiorcomarca=0&cdComarca=&nmComarca=&secoesTreeSelection.values=&secoesTreeSelection.text=&dados.dtJulgamentoInicio=&dados.dtJulgamentoFim=&dados.dtPublicacaoInicio=&dados.dtPublicacaoFim=&dados.origensSelecionadas=T&tipoDecisaoSelecionados=A&dados.ordenarPor=dtPublicacao"
-        yield SplashRequest(url, self.parse, args={'wait': 1})
+        search = getattr(self, "search", None)
+        url = f'https://esaj.tjsp.jus.br/cjsg/resultadoCompleta.do?conversationId=&dados.buscaInteiroTeor={urllib.parse.quote(search)}&dados.pesquisarComSinonimos=S&dados.pesquisarComSinonimos=S&dados.buscaEmenta=&dados.nuProcOrigem=&dados.nuRegistro=&agenteSelectedEntitiesList=&contadoragente=0&contadorMaioragente=0&codigoCr=&codigoTr=&nmAgente=&juizProlatorSelectedEntitiesList=&contadorjuizProlator=0&contadorMaiorjuizProlator=0&codigoJuizCr=&codigoJuizTr=&nmJuiz=&classesTreeSelection.values=&classesTreeSelection.text=&assuntosTreeSelection.values=&assuntosTreeSelection.text=&comarcaSelectedEntitiesList=&contadorcomarca=0&contadorMaiorcomarca=0&cdComarca=&nmComarca=&secoesTreeSelection.values=&secoesTreeSelection.text=&dados.dtJulgamentoInicio=&dados.dtJulgamentoFim=&dados.dtPublicacaoInicio=&dados.dtPublicacaoFim=&dados.origensSelecionadas=T&tipoDecisaoSelecionados=A&dados.ordenarPor=dtPublicacao'
+        yield scrapy.Request(url, self.parse, meta={'selector': '#tdResultados table table'})
 
     def parse(self, response):
-        next_page_script = """
-        function main(splash, args)
-            splash:go(args.url)
+        selector = response.meta.get('selector')
 
-            local a_element = splash:select('[title="Próxima página"]')
-            a_element:mouse_click()
-
-            splash:wait(splash.args.wait)  
-            return splash:html()
-        end
-        """
-
-        for process in response.css('#tdResultados tbody tbody'):
+        for process in response.css(selector):
+            print(process.css('a[title="Visualizar Inteiro Teor"]::text').get().strip())
             yield {
-                'numero_processo': process.css('[title="Visualizar Inteiro Teor"]::text').get().strip(),
+                'numero_processo': process.css('a[title="Visualizar Inteiro Teor"]::text').get().strip(),
                 'comarca': self.get_detail(process, 'tr', 'Comarca:'),
                 'data_julgamento': self.get_detail(process, 'tr', 'Data do julgamento:'),
                 'data_publicacao': self.get_detail(process, 'tr', 'Data de publicação:'),
                 'ementa': treatment(innertext_quick(process.css('tr:last-child div:last-child'))[0]).strip(),
             }
-        if self.has_next_page(response) is not None:
-            yield SplashRequest(
-                response.url,
-                callback=self.parse,
-                endpoint='execute',
-                dont_filter=True,
-                args={'wait': 2, 'lua_source': next_page_script, 'url': response.url}
+
+        logging.info(f"\nURL: {response.url}, Current page: {self.get_current_page(response)}, Has next page: {self.has_next_page(response)}")
+
+        if self.has_next_page(response):
+            sleep(3)
+            next_page = self.next_page(response)
+            next_url = f'https://esaj.tjsp.jus.br/cjsg/trocaDePagina.do?tipoDeDecisao=A&pagina={next_page}'
+            yield scrapy.Request(
+                url=next_url,
+                headers={'Accept': 'text/html; charset=latin1;'},
+                cookies=self.set_cookies(response),
+                meta={'selector': 'table:first-of-type table'},
+                callback=self.parse
             )
+
+    def set_cookies(self, response):
+        cookies = {}
+        for cookie in response.headers.getlist(b'Set-Cookie'):
+            cookie_str = cookie.decode('utf-8')
+            key, value = cookie_str.split('=', 1)
+            cookies[key] = value.split(';', 1)[0]
+        return cookies
 
     def has_next_page(self, response):
         if response.css('[title="Próxima página"]'):
@@ -50,13 +59,8 @@ class CjsgSpider(scrapy.Spider):
     def get_current_page(self, response):
         return int(response.css('.paginaAtual::text').get().strip())
 
-    def get_occurrence_number(self, process):
-        text = process.css('.segredoJustica::text').get(default="").strip()
-        pos = text.find(' ocorrência')
-        occurrence_number = text[1:pos]
-        if occurrence_number:
-            return occurrence_number.strip()
-        return ''
+    def next_page(self, response):
+        return self.get_current_page(response) + 1
 
     def get_detail(self, process, css_selector, search=''):
         element = process.css(f'{css_selector} :contains("{search}")')
@@ -69,5 +73,3 @@ class CjsgSpider(scrapy.Spider):
         if final_text:
             return treatment(final_text).strip()
         return ''
-
-
